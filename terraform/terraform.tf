c86602cf795e0fd530ed1a5c70ab17d816768244
MaheshVellanki <mahivellanki557@gmail.com>
2026-06-16 21:47:30 +0530

    Add multiple AWS instance resources
    Added multiple AWS instance resources with unique tags.

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -4,11 +4,29 @@ provider "aws" {
   secret_key = "SecretKey"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "example1" {
   ami           = "ami-0521cb2d60cfbb1a6" # Amazon Linux 2 AMI (HVM), SSD Volume Type
   instance_type = "t3.micro"
 
   tags = {
-    Name = "TerraformExampleInstance"
+    Name = "TerraformExampleInstance1"
+  }
+}
+
+resource "aws_instance" "example2" {
+  ami           = "ami-0521cb2d60cfbb1a6" # Amazon Linux 2 AMI (HVM), SSD Volume Type
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "TerraformExampleInstance2"
+  }
+}
+
+resource "aws_instance" "example3" {
+  ami           = "ami-0521cb2d60cfbb1a6" # Amazon Linux 2 AMI (HVM), SSD Volume Type
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "TerraformExampleInstance3"
   }
 }
