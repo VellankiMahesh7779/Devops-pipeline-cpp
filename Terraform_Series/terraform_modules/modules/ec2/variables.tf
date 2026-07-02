06fa88ad20258745d8ee4f0e0d39a86bcf1d8890
mahivellanki <mahivellanki557@gmail.com>
2026-07-02 12:01:23 +0000

    Created EC2 instnace by using Modules.

---
+++ Terraform_Series/terraform_modules/modules/ec2/variables.tf
@@ -0,0 +1,9 @@
+variable ami_id {
+  description = "The AMI ID to use for the instance"
+}
+variable instance_type {
+  description = "The type of instance to create"
+}
+variable name {
+  description = "The name of the instance"
+}
