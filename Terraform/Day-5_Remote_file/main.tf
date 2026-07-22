7849b6afb5bf657622dbb19e3bf25dc4f88a1e02
mahivellanki <mahivellanki557@gmail.com>
2026-07-22 12:32:00 +0000

    Added comment  about creating S3 bucket before storing Terraform state

--- Terraform/Day-5_Remote_file/main.tf
+++ Terraform/Day-5_Remote_file/main.tf
@@ -6,3 +6,4 @@ resource "aws_s3_bucket" "my_bucket" {
 bucket = "mahesh-vivi-terraform-s3-bucket-7779"
 
 }
+# first we should create a s3 bucket and then we can use that bucket to store the terraform state file.
